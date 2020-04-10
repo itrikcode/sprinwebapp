ac75e0db8799497ca5f111f88258896eb9a7d798
VIVEK <VIVEK@DESKTOP-MU7HV24>
2020-04-10 01:58:33 -0700

    commit the additionl checkes

--- 13_GitPreactic/src/com/git/Test2.java
+++ 13_GitPreactic/src/com/git/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		String hungr ="yes";
 		
-		if(hungr =="yes")
+		if(hungr =="yes" || hungr=="Y"||  hungr=="y")
 			System.out.println("eat food");
 		else
 			System.out.println("Not requrired ");
