719ed132bb042dc333ae0b53d8cd077c9331df6b
VIVEK <VIVEK@DESKTOP-MU7HV24>
2020-04-10 01:01:18 -0700

    first commit.....

---
+++ 13_GitPreactic/src/com/git/Test1.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		String hungr ="yes";
+		if(hungr =="yess")
+			System.out.println("eat food");
+		else
+			System.out.println("Not requrired ");
+	}
+
+}
