74ea507c2bc7f0de1677feedb19ff655989b36be
VIVEK <VIVEK@DESKTOP-MU7HV24>
2020-04-10 01:32:00 -0700

    commiting test2 to brance developerbrance

---
+++ 13_GitPreactic/src/com/git/Test2.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		String hungr ="yes";
+		
+		if(hungr =="yes")
+			System.out.println("eat food");
+		else
+			System.out.println("Not requrired ");
+	}
+
+}
